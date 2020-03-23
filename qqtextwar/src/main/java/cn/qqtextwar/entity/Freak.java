29b0258164fcca9629f6e3d380bf9be88ee23440
magiclu550 <843983728@qq.com>
2020-03-23 11:52:56 +0800

    anggressivity

--- qqtextwar/src/main/java/cn/qqtextwar/entity/Freak.java
+++ qqtextwar/src/main/java/cn/qqtextwar/entity/Freak.java
@@ -9,16 +9,20 @@ import cn.qqtextwar.math.Vector;
  */
 public abstract class Freak extends Mob implements Skillable,Registered {
 
+    //攻击力
     private int anggressivity;
 
     public Freak(Vector vector,int id,int difficulty) {
         super(vector,id,true,difficulty);
         setManaPoints(initManaPoints(getLevel()));
+        this.anggressivity = initAggressivity(getLevel());
     }
 
     public abstract double initManaPoints(int level);
 
     public abstract int initAggressivity(int level);
 
-
+    public int getAnggressivity() {
+        return anggressivity;
+    }
 }
