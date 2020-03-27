c3926b8a4f155a4cf07c54eee185d3afc5924078
magiclu550 <843983728@qq.com>
2020-03-27 14:17:11 +0800

    client project

---
+++ textwar_client_connector/src/main/java/cn/textwar/client/ClientApplication.java
@@ -0,0 +1,22 @@
+package cn.textwar.client;
+
+import cn.qqtextwar.Server;
+import cn.qqtextwar.api.Application;
+
+public class ClientApplication implements Application {
+
+    @Override
+    public void init(Server server) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void sendMessage(long qq, String message) {
+
+    }
+}
