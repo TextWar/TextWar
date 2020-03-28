715628ce3b584a7cd7feeb4ec5de7aa2101bdbf2
magiclu550 <843983728@qq.com>
2020-03-28 17:36:17 +0800

    fix bug about moving

--- textwar/src/main/java/cn/qqtextwar/command/actions/MoveAction.groovy
+++ textwar/src/main/java/cn/qqtextwar/command/actions/MoveAction.groovy
@@ -2,6 +2,7 @@ package cn.qqtextwar.command.actions
 
 import cn.qqtextwar.command.Action
 import cn.qqtextwar.entity.player.Player
+import cn.qqtextwar.ex.MoveException
 import groovy.transform.CompileStatic
 
 @CompileStatic
@@ -13,22 +14,27 @@ class MoveAction extends Action{
 
     @Override
     String execute(Player player, String command) {
-        if("w" == command.toLowerCase()){
-            player.up(server.gameMap).update()
-            return "${player.id}: move up 1"
+        try{
+            if("w" == command.toLowerCase()){
+                player.up(server.gameMap).update()
+                return "${player.id}: move up 1"
+            }
+            if("s" == command.toLowerCase()){
+                player.down(server.gameMap).update()
+                return "${player.id}: move down 1"
+            }
+            if("a" == command.toLowerCase()){
+                player.left(server.gameMap).update()
+                return "${player.id}: move left 1"
+            }
+            if("d" == command.toLowerCase()){
+                player.right(server.gameMap).update()
+                return "${player.id}: move right 1"
+            }
+        }catch(Exception e){
+            if(e instanceof MoveException){
+                return e.message
+            }
         }
-        if("s" == command.toLowerCase()){
-            player.down(server.gameMap).update()
-            return "${player.id}: move down 1"
-        }
-        if("a" == command.toLowerCase()){
-            player.left(server.gameMap).update()
-            return "${player.id}: move left 1"
-        }
-        if("d" == command.toLowerCase()){
-            player.right(server.gameMap).update()
-            return "${player.id}: move right 1"
-        }
-        return ""
     }
 }
