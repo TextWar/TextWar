20c3f67ac16e4f0c6a4947f1c32bbd7478386887
magiclu550 <843983728@qq.com>
2020-03-30 00:42:04 +0800

    event name

--- textwar_plugin/src/main/java/cn/textwar/plugin/event/Event.java
+++ textwar_plugin/src/main/java/cn/textwar/plugin/event/Event.java
@@ -3,8 +3,14 @@ package cn.textwar.plugin.event;
 public class Event {
 
 
+    private String name;
+
+    public Event(String name) {
+        this.name = name;
+    }
+
     public final String getEventName() {
-        return this.getClass().getName();
+        return name;
     }
 
 
