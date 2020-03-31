edba56f1df87d19c152ad4e6f9b4c6a11cc5ffb6
magiclu550 <843983728@qq.com>
2020-03-31 12:34:20 +0800

    MapHandler

--- textwar_client_connector/src/main/java/cn/textwar/client/ClientServer.java
+++ textwar_client_connector/src/main/java/cn/textwar/client/ClientServer.java
@@ -1,6 +1,7 @@
 package cn.textwar.client;
 
 import cn.qqtextwar.Server;
+import cn.textwar.client.handlers.MapHandler;
 import cn.textwar.protocol.ConnectServer;
 import cn.textwar.protocol.Connecting;
 import cn.textwar.protocol.HandlerExecutor;
@@ -14,6 +15,6 @@ public class ClientServer extends ConnectServer {
 
     @Override
     public void registerHandlers(HandlerExecutor executor) {
-
+        executor.registerHandler(new MapHandler());
     }
 }
---
+++ textwar_client_connector/src/main/java/cn/textwar/client/handlers/MapHandler.java
@@ -0,0 +1,12 @@
+package cn.textwar.client.handlers;
+
+import cn.textwar.protocol.Handler;
+import com.alibaba.fastjson.JSONObject;
+
+public class MapHandler extends Handler {
+
+    @Override
+    public JSONObject execute(String type, JSONObject jsonObject) {
+        return null;
+    }
+}
