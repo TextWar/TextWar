cec0581e77a2f107212827e6559f9ef8e031d54c
magiclu550 <843983728@qq.com>
2020-04-12 21:50:01 +0800

    simple code

--- textwar/src/main/java/cn/qqtextwar/command/Permission.java
+++ textwar/src/main/java/cn/qqtextwar/command/Permission.java
@@ -10,19 +10,8 @@ public enum Permission {
     CONSOLE;
 
     public boolean access(Player player){
-        if(!player.isAdmin()){
-            if(this.name().equals(ALL.name()) || this.name().equals(PLAYER.name())){
-                return true;
-            }
-            if(this.name().equals(ADMIN.name()) || this.name().equals(CONSOLE.name())){
-                return false;
-            }
-            return false;
-        }else{
-            if(this.name().equals(PLAYER.name()) || this.name().equals(CONSOLE.name())){
-                return false;
-            }
-            return true;
-        }
+        return !player.isAdmin()?
+                this.name().equals(ALL.name()) || this.name().equals(PLAYER.name())
+                :!(this.name().equals(PLAYER.name()) || this.name().equals(CONSOLE.name()));
     }
 }
