44640ede41d8f1946c8259e02ba2acacd031f9d5
magiclu550 <843983728@qq.com>
2020-06-19 11:46:32 +0800

    UpdateHandler

--- textwar_client_connector/src/main/java/cn/textwar/client/handlers/UpdateMapHandler.groovy
+++ textwar_client_connector/src/main/java/cn/textwar/client/handlers/UpdateMapHandler.groovy
@@ -17,7 +17,7 @@ import com.alibaba.fastjson.JSONObject
  * }
  * 返回这个地图范围内的实体信息(怪物类型，坐标）
  *
- * 未来添加玩家方法doUpdate,服务端当监测到对应玩家的范围内有实体变动，就会调用其doUpdate更新其范围内的怪物给客户端
+ * 未来添加玩家方法doUpdate,服务端当监测到对应玩家的范围内有实体变动，就会调用其doUpdate方法来更新玩家的客户端
  */
 class UpdateMapHandler extends Handler{
 
